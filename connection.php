af48ab430da55efe11a0cda92d270446a94a061a
Alan Estrada <alan.estrada@udgvirtual.udg.mx>
2023-04-29 17:51:07 -0600

    Cambios para despliegue

--- connection.php
+++ connection.php
@@ -1,12 +1,20 @@
 <?php
 function connection() {
-    $host = 'localhost';
+
+    $DB_HOST = $_ENV['DB_HOST'];
+    $DB_USER = $_ENV['DB_USER'];
+    $DB_PASSWORD = $_ENV['DB_PASSWORD'];
+    $DB_NAME = $_ENV['DB_NAME'];
+    $DB_PORT = $_ENV['DB_PORT'];
+    
+    $db = mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
+/*     $host = 'localhost';
     $user = 'root';
     $password = '';
     $database = 'movies_db';
 
     $conn = mysqli_connect($host, $user, $password);
-    mysqli_select_db($conn,$database);
+    mysqli_select_db($conn,$database); */
     return $conn;
 }
 ?>
